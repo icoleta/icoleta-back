ca17d765c847d194f6a6c16692934909a7faa5f6
Fernando Lins <fernandolins088@gmail.com>
2022-07-20 16:02:24 -0300

    Move Point related methods from CompanyController to PointController

--- app/Http/Controllers/API/PointController.php
+++ app/Http/Controllers/API/PointController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Models\User;
 use App\Models\Point;
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
 
 class PointController extends Controller
 {
@@ -11,6 +14,99 @@ class PointController extends Controller
         $points = Point::all();
         return response()->json([
             'data' => $points,
+        ]);
+    }
+
+    public function listPoints(Request $request) {
+        $user = User::where('email', $request->CompanyEmail)->first();
+        $points = Point::where('company_id', $user->company->id)->get();
+
+        return response()->json([
+            'data' => $points,
+        ]);
+    }
+
+    public function createPoint(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'hours' => 'required',
+            'items' => 'required',
+            'companyEmail' => 'required',
+            // 'license' => 'required',
+        ]);
+
+        if ($validator->fails())
+        {
+            $messages = $validator->messages();
+            return response()->json([
+                'error' => $messages,
+            ], 500);
+        }
+
+        $user = User::where('email', $request->companyEmail)->first();
+        $company = $user->company;
+
+        $point = new Point();
+        $point->name = $request->name;
+        $point->hours = $request->hours;
+        $point->items = $request->items;
+        $point->company_id = $company->id;
+
+        if($point->save()) {
+            return response()->json([
+                'status' => 'Ponto criado com sucesso.',
+                'data' => $point,
+            ]);
+        } else {
+            return response()->json([
+                'error' => "Houve um erro inesperado"
+            ], 500);
+        }
+    }
+
+    public function showPoint($id) {
+        $point = Point::find($id);
+        return response()->json([
+            'data' => $point,
+        ]);
+    }
+
+    public function editPoint(Request $request, $id) {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'hours' => 'required',
+            'items' => 'required',
+        ]);
+
+        if ($validator->fails())
+        {
+            $messages = $validator->messages();
+            return response()->json([
+                'error' => $messages,
+            ], 500);
+        }
+
+        $point = Point::find($id);
+        $point->name = $request->name;
+        $point->hours = $request->hours;
+        $point->items = $request->items;
+
+        if($point->save()) {
+            return response()->json([
+                'status' => 'Ponto editado com sucesso.',
+                'data' => $point,
+            ]);
+        } else {
+            return response()->json([
+                'error' => "Houve um erro inesperado"
+            ], 500);
+        }
+    }
+
+    public function deletePoint($id) {
+        Point::find($id)->delete();
+        return response()->json([
+            'status' => 'Ponto removido com sucesso.',
         ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -29,12 +29,11 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::post('/company/register', [CompanyController::class, 'registerCompany']);
 
 Route::get('point', [PointController::class, 'listAll']);
-
-Route::get('/company/point', [CompanyController::class, 'listPoints']);
-Route::post('/company/point', [CompanyController::class, 'createPoint']);
-Route::get('/company/point/{id}', [CompanyController::class, 'showPoint']);
-Route::put('/company/point/{id}', [CompanyController::class, 'editPoint']);
-Route::delete('/company/point/{id}', [CompanyController::class, 'deletePoint']);
+Route::get('/company/point', [PointController::class, 'listPoints']);
+Route::post('/company/point', [PointController::class, 'createPoint']);
+Route::get('/company/point/{id}', [PointController::class, 'showPoint']);
+Route::put('/company/point/{id}', [PointController::class, 'editPoint']);
+Route::delete('/company/point/{id}', [PointController::class, 'deletePoint']);
 
 Route::post('login', [LoginController::class, 'login']);
 Route::post('logout', [LoginController::class, 'logout']);
