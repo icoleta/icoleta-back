27fa65623c7c1036854b9a54021cff18345269c7
VictorBpt <victorhugo.delimaaraujo@gmail.com>
2022-08-01 20:37:37 -0300

    Ajustes nas funções de ponto da API. Rotas e api não foram modificadas ainda

--- app/Http/Controllers/API/PointController.php
+++ app/Http/Controllers/API/PointController.php
@@ -2,31 +2,36 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Models\User;
-use App\Models\Point;
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Point;
+use App\Models\User;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
 class PointController extends Controller
 {
-    public function listAll() {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
         $points = Point::all();
+        
         return response()->json([
             'data' => $points,
         ]);
     }
 
-    public function listPoints(Request $request) {
-        $user = User::where('email', $request->CompanyEmail)->first();
-        $points = Point::where('company_id', $user->company->id)->get();
-
-        return response()->json([
-            'data' => $points,
-        ]);
-    }
-
-    public function createPoint(Request $request) {
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
         $validator = Validator::make($request->all(), [
             'name' => 'required',
             'hours' => 'required',
@@ -44,13 +49,18 @@ class PointController extends Controller
         }
 
         $user = User::where('email', $request->companyEmail)->first();
-        $company = $user->company;
+        
+        if(!$user){
+            return response()->json([
+                'error' => 'Email não cadastrado!',
+            ], 400);
+        }
 
         $point = new Point();
         $point->name = $request->name;
         $point->hours = $request->hours;
         $point->items = $request->items;
-        $point->company_id = $company->id;
+        $point->company_id = $user->company->id;
 
         if($point->save()) {
             return response()->json([
@@ -64,14 +74,67 @@ class PointController extends Controller
         }
     }
 
-    public function showPoint($id) {
-        $point = Point::find($id);
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $point = Point::where('id', $id)->first();
+        
+        if(!$point){
+            return response()->json(['error' => 'Ponto não encontrado'], 404);
+        }
+
         return response()->json([
             'data' => $point,
         ]);
     }
 
-    public function editPoint(Request $request, $id) {
+    /**
+     * Display all points from a user.
+     * 
+     * @param Request $request
+     * @return Json 
+     */
+    public function showUserPoints(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'CompanyEmail' => 'required',
+        ]);
+
+        if ($validator->fails())
+        {
+            $messages = $validator->messages();
+            return response()->json([
+                'error' => $messages,
+            ], 500);
+        }
+        
+        $user = User::where('email', $request->CompanyEmail)->first();
+        
+        if(!$user){
+            return response()->json([
+                'error' => 'Email não cadastrado!',
+            ], 400);
+        }
+        $points = Point::where('company_id', $user->company->id)->get();
+
+        return response()->json([
+            'data' => $points,
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id) {
         $validator = Validator::make($request->all(), [
             'name' => 'required',
             'hours' => 'required',
@@ -86,7 +149,12 @@ class PointController extends Controller
             ], 500);
         }
 
-        $point = Point::find($id);
+        $point = Point::where('id', $id)->first();
+        
+        if(!$point){
+            return response()->json(['error' => 'Ponto não encontrado'], 404);
+        }
+        
         $point->name = $request->name;
         $point->hours = $request->hours;
         $point->items = $request->items;
@@ -102,11 +170,28 @@ class PointController extends Controller
             ], 500);
         }
     }
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $point = Point::where('id', $id)->first();
+        
+        if(!$point){
+            return response()->json(['error' => 'Ponto não encontrado'], 404);
+        }
 
-    public function deletePoint($id) {
-        Point::find($id)->delete();
-        return response()->json([
-            'status' => 'Ponto removido com sucesso.',
-        ]);
+        if($point->delete()) {
+            return response()->json([
+                'status' => 'Ponto removido com sucesso.',
+            ]);
+        } else {
+            return response()->json([
+                'error' => "Houve um erro inesperado"
+            ], 500);
+        }
     }
 }
