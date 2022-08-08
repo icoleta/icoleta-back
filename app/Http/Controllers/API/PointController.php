927c08b1e09cd0dd9b50cbee6c21e6e17d41cd53
Fernando Lins <fernandolins088@gmail.com>
2022-08-08 18:54:53 -0300

    Update PointController to use collectableItems

--- app/Http/Controllers/API/PointController.php
+++ app/Http/Controllers/API/PointController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Http\Controllers\Controller;
 use App\Models\Point;
-use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 
 class PointController extends Controller
@@ -17,7 +17,7 @@ class PointController extends Controller
      */
     public function index()
     {
-        $points = Point::all();
+        $points = Point::with('collectableItems')->get();
         return response($points, 200);
     }
 
@@ -32,7 +32,7 @@ class PointController extends Controller
         $validator = Validator::make($request->all(), [
             'name' => 'required',
             'hours' => 'required',
-            'items' => 'required',
+            'items' => 'required|array',
         ]);
 
         if($validator->fails())
@@ -41,23 +41,30 @@ class PointController extends Controller
             return response()->json([
                 'error' => $messages,
             ], 500);
-        }
+        }        
 
-        $point = new Point();
-        $point->name = $request->name;
-        $point->hours = $request->hours;
-        $point->phone = $request->phone;
-        $point->longitude = $request->longitude;
-        $point->latitude = $request->latitude;        
-        $point->company_id = $request->user()->company->id;
+        try {
+            DB::transaction(function() use(
+                $request
+            ) {
+                $point = new Point();
+                $point->name = $request->name;
+                $point->hours = $request->hours;
+                $point->phone = $request->phone;
+                $point->longitude = $request->longitude;
+                $point->latitude = $request->latitude;        
+                $point->company_id = $request->user()->company->id;
+                $point->save();
 
-        if($point->save()) {
-            return response(null, 201);
-        } else {
+                $point->collectableItems()->attach($request->items);
+            });
+        } catch (\Throwable $th) {
             return response()->json([
                 'error' => "Houve um erro inesperado"
             ], 500);
         }
+
+        return response(null, 201);
     }
 
     /**
@@ -68,7 +75,7 @@ class PointController extends Controller
      */
     public function show($id)
     {
-        $point = Point::where('id', $id)->first();
+        $point = Point::with('collectableItems')->where('id', $id)->first();
         
         if(!$point){
             return response()->json(['error' => 'Ponto não encontrado'], 404);
@@ -85,7 +92,7 @@ class PointController extends Controller
      */
     public function showCompanyPoints(Request $request)
     {
-        $points = Point::where('company_id', $request->user()->company->id)->get();
+        $points = Point::with('collectableItems')->where('company_id', $request->user()->company->id)->get();
         return response($points);
     }
 
@@ -100,7 +107,7 @@ class PointController extends Controller
         $validator = Validator::make($request->all(), [
             'name' => 'required',
             'hours' => 'required',
-            'items' => 'required',
+            'items' => 'required|array',
         ]);
 
         if ($validator->fails())
@@ -116,19 +123,27 @@ class PointController extends Controller
             return response()->json(['error' => 'Ponto não encontrado'], 404);
         }
         
-        $point->name = $request->name;
-        $point->hours = $request->hours;
-        $point->phone = $request->phone;
-        $point->longitude = $request->longitude;
-        $point->latitude = $request->latitude;        
 
-        if($point->save()) {
-            return response(null, 204);
-        } else {
+        try {
+            DB::transaction(function() use(
+                $request,
+                $point
+            ) {
+                $point->name = $request->name;
+                $point->hours = $request->hours;
+                $point->phone = $request->phone;
+                $point->longitude = $request->longitude;
+                $point->latitude = $request->latitude;
+                $point->save();
+                $point->collectableItems()->sync($request->items);
+            });
+        } catch (\Throwable $th) {
             return response()->json([
                 'error' => "Houve um erro inesperado"
             ], 500);
         }
+
+        return response(null, 204);
     }
     /**
      * Remove the specified resource from storage.
