97b0663863fd507fbc2ea881a721c7eeec09c768
VictorBpt <victorhugo.delimaaraujo@gmail.com>
2022-08-07 23:00:59 -0300

    Ranking finalizado

--- app/Http/Controllers/API/RankingController.php
+++ app/Http/Controllers/API/RankingController.php
@@ -14,54 +14,88 @@ use Illuminate\Http\Request;
 class RankingController extends Controller
 {
     public function ranking(Request $request){
-        $weight = 0;
+        if($request->category == 'weight'){
+            $weight = 0;
 
-        if($request->semester){
-            $semester = Semester::with(['people' => function($query){ return $query->with('discards');}])->get();
-
-            $semester->each(function ($semester) use ($weight){
-                
-                foreach($semester->people as $person){
-                    $personDiscardWeight = 0;
-                    foreach($person->discards as $discard){
-                        $personDiscardWeight += $discard->weight;
+            if($request->type && $request->type == 'semester'){
+                $semester = Semester::with(['people' => function($query){ return $query->with('discards');}])->get();
+    
+                $semester->each(function ($semester) use ($weight){
+                    
+                    foreach($semester->people as $person){
+                        $personDiscardWeight = 0;
+                        foreach($person->discards as $discard){
+                            $personDiscardWeight += $discard->weight;
+                        }
+                        $weight += $personDiscardWeight;
                     }
-                    $weight += $personDiscardWeight;
-                }
-                unset($semester->people);
-                $semester->weight = $weight;
-            });
-
-            return response()->json(['data'=> ['semester' =>$semester]]);
-        }elseif($request->course){
-            $courses = Courses::with(['people' => function($query){ return $query->with('discards');}])->get();
-
-            $courses->each(function ($course) use ($weight){
-                
-                foreach($course->people as $person){
-                    $personDiscardWeight = 0;
-                    foreach($person->discards as $discard){
-                        $personDiscardWeight += $discard->weight;
+                    unset($semester->people);
+                    $semester->sum = $weight;
+                    $weight = 0;
+                });
+    
+                return response()->json(['data'=> ['semester' =>$semester]]);
+            }elseif($request->type && $request->type == 'course'){
+                $courses = Courses::with(['people' => function($query){ return $query->with('discards');}])->get();
+    
+                $courses->each(function ($course) use ($weight){
+                    
+                    foreach($course->people as $person){
+                        $personDiscardWeight = 0;
+                        foreach($person->discards as $discard){
+                            $personDiscardWeight += $discard->weight;
+                        }
+                        $weight += $personDiscardWeight;
                     }
-                    $weight += $personDiscardWeight;
-                }
-                unset($course->people);
-                $course->weight = $weight;
-            });
+                    unset($course->people);
+                    $course->sum = $weight;
+                    $weight = 0;
+                });
+    
+                return response()->json(['data'=> ['courses' =>$courses]]);
+    
+            }else {
+                $ranking = Discard::with(['person' => function($query){ return $query->with('course');}])->selectRaw('person_id, sum(weight) as sum')->orderBy('sum', 'desc')->groupBy('person_id')->get();            
+    
+                return response()->json(['data'=> ['ranking' =>$ranking]]);
+            }
+        }else{
+            $count = 0;
 
-            return response()->json(['data'=> ['courses' =>$courses]]);
-
-        }else {
-            $ranking = Discard::with(['person' => function($query){ return $query->with('course');}])->selectRaw('person_id, sum(weight) as sum')->orderBy('sum', 'desc')->groupBy('person_id')->get();            
-
-            return response()->json(['data'=> ['ranking' =>$ranking]]);
+            if($request->type && $request->type == 'semester'){
+                $semester = Semester::with(['people' => function($query){ return $query->with('discards');}])->get();
+    
+                $semester->each(function ($semester) use ($count){
+                    
+                    foreach($semester->people as $person){
+                        $count += count($person->discards);
+                    }
+                    unset($semester->people);
+                    $semester->sum = $count;
+                    $count = 0;
+                });
+    
+                return response()->json(['data'=> ['semester' =>$semester]]);
+            }elseif($request->type && $request->type == 'course'){
+                $courses = Courses::with(['people' => function($query){ return $query->with('discards');}])->get();
+    
+                $courses->each(function ($course) use ($count){
+                    
+                    foreach($course->people as $person){
+                        $count += count($person->discards);
+                    }
+                    unset($course->people);
+                    $course->sum = $count;
+                    $count = 0;
+                });
+    
+                return response()->json(['data'=> ['courses' =>$courses]]);
+    
+            }else {
+                $ranking = Discard::with(['person' => function($query){ return $query->with('course');}])->selectRaw('person_id, count(weight) as sum')->orderBy('sum', 'desc')->groupBy('person_id')->get();            
+    
+                return response()->json(['data'=> ['ranking' =>$ranking]]);
+            }
         }
-
-    }
-
-    public function rankingByDiscardCount(){
-        $topTen = Discard::with('person')->selectRaw('person_id, name, count(weight) as count')->orderBy('count', 'desc')->groupBy('person_id')->limit(10)->get();
-
-        return response()->json($topTen);
     }
 }
