72015733f78dbd5da4a45520e77d40a6888ef153
Fernando Lins <fernandolins088@gmail.com>
2022-08-08 17:38:30 -0300

    style: clear commented code on login method

--- app/Http/Controllers/Auth/API/LoginController.php
+++ app/Http/Controllers/Auth/API/LoginController.php
@@ -3,16 +3,14 @@
 namespace App\Http\Controllers\Auth\API;
 
 use App\Http\Controllers\Controller;
-use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
 class LoginController extends Controller
 {
     public function login(Request $request){
         $validator = Validator::make($request->all(), [
-            'email' => 'required',
+            'email' => 'required|email',
             'password' => 'required',
         ]);
 
@@ -24,7 +22,6 @@ class LoginController extends Controller
             ], 500);
         }
 
-        // $login = User::where('email', $request->email)->first();
         $credentials = $request->only('email', 'password');
 
         if(!auth()->attempt($credentials))
@@ -35,15 +32,5 @@ class LoginController extends Controller
         $token = auth()->user()->createToken($request->email);
         auth()->user()->token = $token->plainTextToken;
         return response()->json(['data' => auth()->user()]);
-        // if ($login && Hash::check($request->password, $login->password)) {
-        //     return response()->json([
-        //         'status' => 'Usuário logado com sucesso.',
-        //         'data' => $login,
-        //     ]); 
-        // } else {
-        //     return response()->json([
-        //         'error' => "Dados inválidos!"
-        //     ], 401);
-        // }
     }
 }
