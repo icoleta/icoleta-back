1d6c786016bef8df819534e640c6a9733c9e377e
Fernando Lins <fernandolins088@gmail.com>
2022-08-10 15:43:46 -0300

    Add person, point & residuum relations to Discard

--- app/Models/Discard.php
+++ app/Models/Discard.php
@@ -9,8 +9,16 @@ class Discard extends Model
 {
     use HasFactory;
 
-    public function user()
+    public function person()
     {
-        return $this->belongsTo(User::class, 'user_id', 'id');
+        return $this->belongsTo(Person::class, 'person_id', 'id');
+    }
+
+    public function point() {
+        return $this->belongsTo(Point::class, 'point_id', 'id');
+    }
+
+    public function residuum() {
+        return $this->belongsTo(Residuum::class, 'residuum_id', 'id');
     }
 }
