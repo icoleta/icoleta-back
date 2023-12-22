7b40f7f12dc9cc1c5bce3fac5e5e37829e822bb5
Fernando Lins <fernandolins088@gmail.com>
2023-12-22 11:09:43 -0300

    WIP

--- app/Http/Controllers/API/DiscardController.php
+++ app/Http/Controllers/API/DiscardController.php
@@ -8,8 +8,8 @@ use App\Models\Person;
 use App\Models\Residuum;
 use App\Models\User;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
 
 class DiscardController extends Controller
 {
@@ -39,7 +39,7 @@ class DiscardController extends Controller
             'residuum_id' => 'required',
         ]);
 
-        if($validator->fails()) {
+        if ($validator->fails()) {
             $messages = $validator->messages();
             return response()->json([
                 'error' => $messages,
@@ -47,12 +47,12 @@ class DiscardController extends Controller
         }
 
         try {
-            DB::transaction(function() use (
+            DB::transaction(function () use (
                 $request
             ) {
                 $person = User::where('email', $request->email)->first()->person;
 
-                if(!$person)
+                if (!$person)
                     return response()->json([
                         'error' => 'Usuário não encontrado!'
                     ], 404);
@@ -63,7 +63,76 @@ class DiscardController extends Controller
                 $discard->residuum_id = $request->residuum_id;
                 $discard->weight = $request->weight;
                 $discard->save();
+
+                return response()->json([
+                    'data' => $discard,
+                    'status' => 'Descarte cadastrado!'
+                ], 201);
+            });
+        } catch (\Throwable $th) {
+            return response()->json([
+                'error' => 'Houve um erro inesperado!'
+            ], 500);
+        }
+    }
+
+    public function createDiscardAsUser(Request $request)
+    {
+        $data = $this->getTokenData(request('token'));
+
+        return response()->json([
+            'error' => $data
+        ], 500);
     
+        $validator = Validator::make($request->all(), [
+            'email' => 'required',
+            'weight' => 'required',
+            'point_id' => 'required|exist:points,id',
+            'residuum_id' => [
+                'required',
+                function ($attribute, $value, $fail) use ($request) {
+                    $pointId = $request->input('point_id');
+                    $exists = DB::table('point_residuum')
+                        ->where('residuum_id', $value)
+                        ->where('point_id', $pointId)
+                        ->exists();
+                    if (!$exists) {
+                        $fail('Invalid point_id or residuum_id.');
+                    }
+                },
+            ],
+        ]);
+
+        return response()->json([
+            'error' => 42
+        ], 500);
+
+
+        if ($validator->fails()) {
+            $messages = $validator->messages();
+            return response()->json([
+                'error' => $messages,
+            ], 500);
+        }
+
+        try {
+            DB::transaction(function () use (
+                $request
+            ) {
+                $person = User::where('email', $request->email)->first()->person;
+
+                if (!$person)
+                    return response()->json([
+                        'error' => 'Usuário não encontrado!'
+                    ], 404);
+
+                $discard = new Discard();
+                $discard->person_id = $person->id;
+                $discard->point_id = $request->point_id;
+                $discard->residuum_id = $request->residuum_id;
+                $discard->weight = $request->weight;
+                $discard->save();
+
                 return response()->json([
                     'data' => $discard,
                     'status' => 'Descarte cadastrado!'
@@ -85,8 +154,8 @@ class DiscardController extends Controller
     public function show($id)
     {
         $discard = Discard::where('id', $id)->first();
-        
-        if(!$discard){
+
+        if (!$discard) {
             return response()->json(['error' => 'Descarte não encontrado!'], 404);
         }
 
@@ -114,11 +183,11 @@ class DiscardController extends Controller
     public function destroy($id)
     {
         $discard = Discard::find($id);
-        if(!$discard){
+        if (!$discard) {
             return response()->json(['error' => 'Descarte não encontrado!'], 404);
         }
 
-        if($discard->delete()) {
+        if ($discard->delete()) {
             return response()->json(['status' => 'Descarte removido!'], 200);
         } else {
             return response()->json([
@@ -127,22 +196,23 @@ class DiscardController extends Controller
         }
     }
 
-    public function listUserDiscards(Request $request) {
+    public function listUserDiscards(Request $request)
+    {
         $personId = Person::where('user_id', $request->user()->id)->first()->id;
-        
+
         $userDiscards = Discard::with(['residuum', 'point'])->where('person_id', $personId)->get();
         $totalWeightDiscarded = 0;
         $summaryByResiduum = [];
-        
+
         // Initializes count and weight by residuum
         $residuumTypes = Residuum::orderBy('name')->get();
-        foreach($residuumTypes as $residuum) {
+        foreach ($residuumTypes as $residuum) {
             $summaryByResiduum[$residuum->name]['count'] = 0;
             $summaryByResiduum[$residuum->name]['weight'] = 0;
         }
 
         // Sums the total discarded weight and the count and weight per residuum discarded
-        foreach($userDiscards as $discard) {
+        foreach ($userDiscards as $discard) {
             $totalWeightDiscarded += $discard->weight;
             $summaryByResiduum[$discard->residuum->name]['count'] += 1;
             $summaryByResiduum[$discard->residuum->name]['weight'] += $discard->weight;
@@ -154,5 +224,29 @@ class DiscardController extends Controller
             'summaryByResiduum' => $summaryByResiduum,
             'discards' => $userDiscards,
         ]);
+    }
+
+    private function getTokenData($token)
+    {
+        $secretKey = 'umasenha';
+        
+        list($headerEncoded, $payloadEncoded, $signature) = explode('.', $token);
+        
+        $payload = json_decode(base64_decode($payloadEncoded));
+        
+        $computedSignature = hash_hmac('sha256', "$headerEncoded.$payloadEncoded", $secretKey, true);
+        $computedSignatureBase64 = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($computedSignature));
+
+        if ($computedSignatureBase64 !== $signature) {
+            return null;
+        }
+
+        $weight = $payload->weight;
+        $residuum_id = $payload->residuum_id;
+
+        return [
+            $weight,
+            $residuum_id,
+        ];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -11,6 +11,7 @@ use App\Http\Controllers\API\ResiduumController;
 use App\Http\Controllers\API\SemesterController;
 use App\Http\Controllers\API\RankingController;
 use App\Http\Controllers\Auth\API\LoginController;
+use App\Http\Controllers\FeedbackController;
 
 /*
 |--------------------------------------------------------------------------
@@ -43,17 +44,19 @@ Route::get('ranking', [RankingController::class, 'ranking']);
 Route::get('semester', [SemesterController::class, 'index']);
 Route::get('course', [CourseController::class, 'index']);
 Route::get('/residuum', [ResiduumController::class, 'index']);
+Route::post('feedback', [FeedbackController::class, 'store']);
 
 Route::post('login', [LoginController::class, 'login']);
+Route::post('discards', [DiscardController::class, 'createDiscardAsUser']);
 
 // Private Routes
-Route::group(['middleware' => ['auth:sanctum']], function() {
+Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post('logout', [LoginController::class, 'logout']);
 
     Route::get('/person/discards', [DiscardController::class, 'listUserDiscards']);
-    
-    Route::middleware('ensureCompany')->prefix('/company')->group(function() {
-        Route::prefix('/point')->group(function() {
+
+    Route::middleware('ensureCompany')->prefix('/company')->group(function () {
+        Route::prefix('/point')->group(function () {
             Route::post('/', [PointController::class, 'store']);
             Route::get('/{id}', [PointController::class, 'show']);
             Route::put('/{id}', [PointController::class, 'update']);
@@ -61,27 +64,27 @@ Route::group(['middleware' => ['auth:sanctum']], function() {
             Route::get('/', [PointController::class, 'showCompanyPoints']);
         });
     });
-    
-    Route::middleware('ensureAdmin')->prefix('/admin')->group(function() {
-        Route::prefix('/person')->group(function() {
+
+    Route::middleware('ensureAdmin')->prefix('/admin')->group(function () {
+        Route::prefix('/person')->group(function () {
             Route::get('/', [PersonController::class, 'index']);
             Route::patch('/{id}', [PersonController::class, 'makeVolunteer']);
         });
 
-        Route::prefix('discards')->group(function(){
+        Route::prefix('discards')->group(function () {
             Route::get('/', [DiscardController::class, 'index']);
             Route::post('/', [DiscardController::class, 'store']);
             Route::get('/{id}', [DiscardController::class, 'show']);
             Route::delete('/{id}', [DiscardController::class, 'delete']);
         });
 
-        Route::prefix('/company')->group(function() {
+        Route::prefix('/company')->group(function () {
             Route::get('/', [CompanyController::class, 'index']);
             Route::get('/{id}', [CompanyController::class, 'show']);
             Route::patch('/{id}', [CompanyController::class, 'verify']);
         });
 
-        Route::prefix('/residuum')->group(function() {
+        Route::prefix('/residuum')->group(function () {
             Route::post('/', [ResiduumController::class, 'store']);
             Route::put('/{id}', [ResiduumController::class, 'edit']);
             Route::delete('/{id}', [ResiduumController::class, 'delete']);
